d7fdef75920904673c7c20682ec36dd8936a9239
Gleb Devyatkin <sovgvd@gmail.com>
2020-03-22 18:31:44 +1300

    back/hind leg servo orientation update

--- software/robot_dog_esp32/robot_dog_esp32.ino
+++ software/robot_dog_esp32/robot_dog_esp32.ino
@@ -80,7 +80,7 @@ leg legs[LEG_NUM] = {
     },
     {-LEG_POINT_X, -LEG_POINT_Y, -LEG_POINT_Z},
     {LEG_ANGLE_ALPHA, LEG_ANGLE_BETA, LEG_ANGLE_GAMMA},
-    {true, true, false, false, false, true},
+    {true, true, false, false, true, true},
     {true, 39, 2000}
   },
   // RIGHT HIND
@@ -97,7 +97,7 @@ leg legs[LEG_NUM] = {
     },
     {LEG_POINT_X, -LEG_POINT_Y, -LEG_POINT_Z},
     {LEG_ANGLE_ALPHA, LEG_ANGLE_BETA, LEG_ANGLE_GAMMA},
-    {false, true, false, false, false, true},
+    {false, true, false, true, false, false},
     {true, 36, 2000}
   }
 };
